ac0ab483950143c864de6a19b18481524d24a320
paradoxirony <coderbhaiya@gmail.com>
2024-05-04 01:00:39 +0530

    done

---
+++ numpywitcv2.py
@@ -0,0 +1,17 @@
+#from PIL import Image as im
+import numpy as np
+#import cv2
+#array_create = np.full((500,500,3),255,dtype=np.uint8)
+#array_create[:,:] =[255,25,255]
+#cv2.imshow('image',array_create)
+#new_arr = np.full((500,500,3),255,dtype = np.uint8)
+#new_arr[:,:] = [255,20,20]
+#cv2.imshow('dishu',new_arr)
+#imagw = im.open(r'E:\class_work\numpy\numpy_practise\prototype of website.png')
+#print(imagw.format)
+#print(imagw.size)
+#print(imagw.mode)
+#numpydata = np.array(imagw)
+#print(numpydata.shape)
+
+
